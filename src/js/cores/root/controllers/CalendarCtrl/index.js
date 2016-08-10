397c33eb2a37f5f48718bf56c739c983aa0b84a4
Tkach7 <dan-tkach@yandex.ru>
2016-08-10 23:25:43 +0500

    fix calendar

--- src/js/cores/root/controllers/CalendarCtrl/index.js
+++ src/js/cores/root/controllers/CalendarCtrl/index.js
@@ -3,22 +3,40 @@ module.exports = angular.module('app').controller('CalendarCtrl', controller);
 function controller($scope, Calendar)  {
         $scope.calendar = Calendar;
         $scope.increaseYear = () => {
-            Calendar.increaseYear();
-            $scope.weekdaysWithNum = Calendar.weekdaysWithNum;
+            $scope.calendar.currentYear = String(+$scope.calendar.currentYear + 1);
+            $scope.weekdaysWithNum = Calendar.getWeekdaysWithNum(Calendar.currentYear, Calendar.currentMonth);
         };
         $scope.reduceYear = () => {
-            Calendar.reduceYear();
-            $scope.weekdaysWithNum = Calendar.weekdaysWithNum;
+            $scope.calendar.currentYear = +$scope.calendar.currentYear > 0 ?
+                String(+$scope.calendar.currentYear - 1)
+                :
+                $scope.calendar.currentYear;
+            $scope.weekdaysWithNum = Calendar.getWeekdaysWithNum(Calendar.currentYear, Calendar.currentMonth);
         };
         $scope.increaseMonth = () => {
-            Calendar.increaseMonth();
-            $scope.weekdaysWithNum = Calendar.weekdaysWithNum;
+            $scope.calendar.currentMonth = +$scope.calendar.currentMonth  < 12 ?
+                String(+$scope.calendar.currentMonth + 1)
+                :
+                $scope.calendar.currentMonth;
+                $scope.calendar.currentMonth = +$scope.calendar.currentMonth < 10 ?
+                    '0' + $scope.calendar.currentMonth
+                    :
+                    $scope.calendar.currentMonth;
+            $scope.weekdaysWithNum = Calendar.getWeekdaysWithNum(Calendar.currentYear, Calendar.currentMonth);
         };
         $scope.reduceMonth = () => {
-            Calendar.reduceMonth();
-            $scope.weekdaysWithNum = Calendar.weekdaysWithNum;
+            $scope.calendar.currentMonth = +$scope.calendar.currentMonth > 0 ?
+                String(+$scope.calendar.currentMonth - 1)
+                :
+                $scope.calendar.currentMonth;
+                $scope.calendar.currentMonth = +$scope.calendar.currentMonth < 10 ?
+                    '0' + $scope.calendar.currentMonth
+                    :
+                    $scope.calendar.currentMonth;
+            $scope.weekdaysWithNum = Calendar.getWeekdaysWithNum(Calendar.currentYear, Calendar.currentMonth);
         }
+
         $scope.daysOfTheWeek = ['Mo','Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-        $scope.weekdaysWithNum = Calendar.weekdaysWithNum;
+        $scope.weekdaysWithNum = Calendar.getWeekdaysWithNum(Calendar.currentYear, Calendar.currentMonth);
  };
 
--- src/js/services/calendar.js
+++ src/js/services/calendar.js
@@ -1,32 +1,6 @@
 module.exports = angular.module('app').service('Calendar', function() {
     this.currentYear = moment().format('YYYY');
     this.currentMonth = moment().format('MM');
-    this.increaseYear = () => {
-        let year = +this.currentYear;
-        year += 1;
-        this.currentYear = String(year);
-        this.weekdaysWithNum = this.getWeekdaysWithNum(this.currentYear, this.currentMonth);
-    };
-    this.reduceYear = () => {
-        let year = +this.currentYear;
-        year = year < 0 ? year : year - 1;
-        this.currentYear = String(year);
-        this.weekdaysWithNum = this.getWeekdaysWithNum(this.currentYear, this.currentMonth);
-    };
-    this.increaseMonth = () => {
-        let month = +this.currentMonth;
-        month = month > 11 ? month : month + 1;
-        month = month < 10 ? '0' + month : month;
-        this.currentMonth = String(month);
-        this.weekdaysWithNum = this.getWeekdaysWithNum(this.currentYear, this.currentMonth);
-    };
-    this.reduceMonth = () => {
-        let month = +this.currentMonth;
-        month = month < 2 ? month : month - 1;
-        month = month < 10 ? '0' + month : month;
-        this.currentMonth = String(month);
-        this.weekdaysWithNum = this.getWeekdaysWithNum(this.currentYear, this.currentMonth);
-    };
     this.getWeekdaysWithNum = (year, month) => {
         var sortMonth = {
             Mo: [],
@@ -40,21 +14,51 @@ module.exports = angular.module('app').service('Calendar', function() {
         var dateFormat = year.concat('-', month);
         var startPeriod = moment(dateFormat).startOf('month');
         var countDaysInMonth = startPeriod.daysInMonth();
-        var dayName, day;
-        for (var i = 0; i < countDaysInMonth; i++) {
-            day = startPeriod.format('DD');
+        var dayName, dayNum;
+        for (let i = 0; i < countDaysInMonth; i++) {
+            dayNum = startPeriod.format('DD');
             dayName = startPeriod.format('dd');
-            sortMonth[dayName].push(day);
+            sortMonth[dayName].push(dayNum);
             startPeriod = startPeriod.add(1, 'day');
         };
+        // add days previous month
         if (sortMonth.Mo[0] != '01') {
-            let currentDay = moment().startOf('week').add(1, 'day');
-            while (sortMonth[currentDay.format('dd')][0] != '01') {
-                sortMonth[currentDay.format('dd')].unshift('.');
-                currentDay = currentDay.add(1, 'day');
+            let countNotStartDays = 0;
+            for (let day in sortMonth) {
+                if (sortMonth[day][0] === '01') break;
+                countNotStartDays += 1;
+            }
+            let nextDay = moment().startOf('week').add(1, 'day');
+            let previousDay = moment()
+                .endOf('month')
+                .subtract(1, 'month')
+                .subtract(countNotStartDays - 1,'day').format('DD');
+            while (sortMonth[nextDay.format('dd')][0] != '01') {
+                sortMonth[nextDay.format('dd')].unshift(previousDay);
+                nextDay = nextDay.add(1, 'day');
+                previousDay = String(+previousDay + 1);
             }
         }
+        // add days next month
+        if (sortMonth.Su[sortMonth.Su.length - 1] != String(countDaysInMonth)) {
+            let indexLastDay = 1;
+            for (let day in sortMonth) {
+                if (sortMonth[day][sortMonth[day].length - 1] === String(countDaysInMonth)) break;
+                indexLastDay += 1;
+            }
+            let dayInNextMonth = '01';
+            startPeriod = moment().startOf('month').format('dd');
+            for (let i = 1; i <= 7; i++) {
+                if (i <= indexLastDay) {
+                    startPeriod = moment().startOf('month').add(i, 'day').format('dd');
+                } else {
+                    sortMonth[startPeriod].push(dayInNextMonth);
+                    dayInNextMonth = '0' + (+dayInNextMonth + 1);
+                    startPeriod = moment().startOf('month').add(i, 'day').format('dd');
+                }
+            }
+        }
+
         return sortMonth;
     };
-    this.weekdaysWithNum = this.getWeekdaysWithNum(this.currentYear, this.currentMonth);
 });
